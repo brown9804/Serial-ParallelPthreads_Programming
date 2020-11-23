767c6b2a7da28e153f3ba711c9ecea41a2a21054
Belinda Brown <belindabrownr04@gmail.com>
2020-11-22 23:44:16 -0600

    pi calculator implementation -  ready

--- src/main.c
+++ src/main.c
@@ -1,8 +1,3 @@
-
-
-#ifndef MAIN
-#define MAIN
-
 // ##--------------------------------main file------------------------------------
 // ##
 // ## Copyright (C) 2020 by 
@@ -16,21 +11,27 @@
 // INCLUDE
 #include <stdio.h>
 #include <stdlib.h>
+#include "../testers/t_picalculator.c"
+
 ///////////////////////////////
 
-int main(void){
+int main(){
 
     printf("\n***********************************************************************\n");
     printf("		PI CALCULATOR	");
     printf("\n***********************************************************************\n");
 
-    int picalculator_status = 0;//t_picalculator();
+    int picalculator_status = t_picalculator(); 
 
     if (picalculator_status == 0)
     {
         printf("\n-----------------------------------------------\n");
         printf("PI CALCULATOR --- Status ------->  Serial & Paralel are:\nSAME results");
         printf("\n-----------------------------------------------\n");
+        printf("\n      .' '.               __               ");
+        printf("\n .        .   .          (__}'         ");
+        printf("\n .         .         . -{{_(|8)         ");
+        printf("\n' .  . ' ' .  . '        (__}'             \n");
         return EXIT_SUCCESS;
     }
     else
@@ -38,6 +39,11 @@ int main(void){
         printf("\n-----------------------------------------------\n");
         printf("PI CALCULATOR --- Status ------->  Serial & Paralel are:\nNOT the same results");
         printf("\n-----------------------------------------------\n");
+        printf("\n      .' '.               __               ");
+        printf("\n .        .   .          (__}'         ");
+        printf("\n .         .         . -{{_(|8)         ");
+        printf("\n' .  . ' ' .  . '        (__}'             \n");
+        printf("\nIs going to be a  EXIT_FAILURE\n");
         exit(EXIT_FAILURE);
     }
 
@@ -50,16 +56,17 @@ int main(void){
     if (vecmatrix_status == 0)
     {
         printf("\n-----------------------------------------------\n");
-        printf("MATRIX & VECTORS (*) CALCULATOR --- Status -------> \n Serial & Paralel are:\nSAME results");
+        printf("\nMATRIX & VECTORS (*) CALCULATOR --- Status -------> \n Serial & Paralel are:\nSAME results");
         printf("\n-----------------------------------------------\n");
         return EXIT_SUCCESS;
     }
     else
     {
         printf("\n-----------------------------------------------\n");
-        printf("MATRIX & VECTORS (*) CALCULATOR --- Status ------->  \nSerial & Paralel are:\nNOT the same results");
+        printf("\nMATRIX & VECTORS (*) CALCULATOR --- Status ------->  \nSerial & Paralel are:\nNOT the same results");
         printf("\n-----------------------------------------------\n");
+        printf("\nIs going to be a  EXIT_FAILURE\n");
         exit(EXIT_FAILURE);
     }
 }
-#endif
+
