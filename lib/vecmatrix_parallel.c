d27e6cb9bdec34b52406bda1be1fa56933f34293
brown9804 <belindabrownr04@gmail.com>
2020-11-23 19:59:23 -0600

    dixing comment in serial and initial parallel

--- lib/vecmatrix_parallel.c
+++ lib/vecmatrix_parallel.c
@@ -1,23 +1,29 @@
-// ##-------------------------------- vecmatrix_serial file------------------------------------
+// ##-------------------------------- vecmatrix_parallel file------------------------------------
 // ## Copyright (C) 2020 by
 // ## Belinda Brown Ramírez
 // ## timna.brown@ucr.ac.cr
-// ##	Multiplication of vectors and matrices - Serial
+// ##	Multiplication of vectors and matrices - Parallel
 // ##-----------------------------------------------------------------------------
 
 // ##########   IMPORTING  PACKAGES ##########
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
 
 // ##########DEFINITIONS OF NECESSARY FUNCTIONS##########
 // Characteristics
 clock_t start, end;
 
+// ##########DEFINITIONS OF NECESSARY FUNCTIONS##########
+// Characteristics
+#define AMOUNT_THREADS 8
+#define ITER 100
+
 // ########## DEFINITION ##########
 //1. Create random matriz and random vector
 //2. nx1 X nxn, where n in range of [3, 8]
-int vecmatrix_serial()
+int vecmatrix_parallel()
 {
     // ########## VARIABLES ##########
     int i = 0;
@@ -46,10 +52,11 @@ int vecmatrix_serial()
     int matrix[n][n];
     int vector[n][1];
     int matrix_result[n][n]; // mxn X nxm =  mxm
-    ///////////////////////
+    int avg_matrix_result[n][n];
+        ///////////////////////
 
-    // Matrix created
-    for (i = 0; i < n; i++) // row
+        // Matrix created
+        for (i = 0; i < n; i++) // row
     {
         for (j = 0; j < n; j++) // column
         {
@@ -101,22 +108,41 @@ int vecmatrix_serial()
     }
 
     ////////////////////////////////////
+    /////// PARALLEL 
     /////// MULTIPLICATION OF MATRIX
     ///////////////////////////////////
-    for (int a = 0; a < 1; a++) // columns vector
-    {
-        for (int i = 0; i < n; i++) // rows matrix
-        {
-            int sum = 0;
-            for (int j = 0; j < n; j++) // column matrix
-            {
-                // multiply and sum
-                sum += matrix[i][j] * vector[j][a];
-            }
+    // ########## OP ##########
+    pthread_mutex_t mutex; // MUTual EXclusion
+    int iter_pthreads = ITER * AMOUNT_THREADS;
 
-            matrix_result[i][a] = sum;
+    while (n < iter_pthreads)
+    {
+        for (int a = 0; a < 1; a++) // columns vector
+        {
+            for (int i = 0; i < n; i++) // rows matrix
+            {
+                int sum = 0;
+                for (int j = 0; j < n; j++) // column matrix
+                {
+                    // multiply and sum
+                    sum += matrix[i][j] * vector[j][a];
+                }
+
+                matrix_result[i][a] = sum;
+          
+            }
         }
+        avg_matrix_result += matrix_result;
+        pthread_mutex_lock(&mutex);
+        avg_matrix_result = avg_matrix_result / iter_pthreads;
+        pthread_mutex_unlock(&mutex);
+        // FOR TESTS
+        printf("\nMatrix X Vector Calculation #%d \nResult using PARALLEL calculation:                  %f", n, matrix_result[i][a]);
+        n++;
     }
+
+
+
 
     printf("\n***************************\n");
     printf("		MULTIPLICATION RESULT 	");
@@ -126,10 +152,10 @@ int vecmatrix_serial()
     {
         for (int j = 0; j < n; j++) // column vector
         {
-            printf("%d ", matrix_result[i][j]);
+            printf("%d ", avg_matrix_result[i][j]);
         }
         printf("\n");
     }
 
-    return matrix_result[i][j];
-}
+    return avg_matrix_result[i][j];
+    }
