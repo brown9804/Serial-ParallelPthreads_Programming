c4b786fad32911d9f51eedc5051c57b7afceaab5
brown9804 <belindabrownr04@gmail.com>
2020-11-25 10:27:26 -0600

    Adding run file and ex from test pipeline

---
+++ run_file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+##------------RUN PROJECT JOB------------------------------------
+## Copyright (C) 2020 by Brown, Belinda.
+## timna.brown@ucr.ac.cr
+##-------------------------------------------------------------------
+
+#### COMPILE 
+make
+# gcc -pthread src/main.c -o main.o -lm 2> /dev/null
+
+# #### RUN
+# chmod -R o+x ./
+# ./main #2> /dev/null
